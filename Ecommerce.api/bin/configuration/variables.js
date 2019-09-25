9166848969cffced07404d85f4efaaa972a8fac1
Kemuel <kemutioficial@gmail.com>
2019-09-25 20:54:16 -0300

    Finalizando Api_Autenticação

--- Ecommerce.api/bin/configuration/variables.js
+++ Ecommerce.api/bin/configuration/variables.js
@@ -6,6 +6,9 @@ const variables = {
     Database:{
    connection: process.env.connection || 'mongodb+srv://admin:vitoria@cluster0-wj6vr.mongodb.net/test?retryWrites=true&w=majority'
 
-    }   
+    },
+    Security: {
+        secretKey: 'd41d8cd98f00b204e9800998ecf8427e|dde975e926b5e37179830dd1038aaeda'
+    }
 }
 module.exports = variables;
